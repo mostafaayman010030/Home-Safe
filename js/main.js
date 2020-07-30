20d51722e4a77dafb4d5be73910dda85ea9f8fe2
mostafa ayman ahmed <mostafaayman010030@gmail.com>
2020-07-30 06:13:14 +0200

    Homesafe-5p

---
+++ js/main.js
@@ -0,0 +1,74 @@
+// Fixed Scroll 
+$(document).scroll(function () {
+    var y = $(this).scrollTop();
+    if (y > 175) {
+        $('.fixed').slideDown();
+    } else {
+        $('.fixed').fadeOut();
+    }
+});
+
+// Testimonials
+(function autoSlider() {
+    $('.slider .active').each(function () {
+
+        if (!$(this).is(':last-child')) {
+
+            $(this).delay(3000).fadeOut(1000, function () {
+
+                $(this).removeClass('active').next().addClass('active').fadeIn();
+
+                autoSlider();
+
+            });
+        } else {
+
+            $(this).delay(3000).fadeOut(1000, function () {
+
+                $(this).removeClass('active');
+
+                $('.slider div').eq(0).addClass('active').fadeIn();
+
+                autoSlider();
+
+            });
+        }
+    });
+}());
+
+
+
+
+//////////////////
+jQuery(document).ready(function($){
+	// browser window scroll (in pixels) after which the "back to top" link is shown
+	var offset = 300,
+		//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+		offset_opacity = 1200,
+		//duration of the top scrolling animation (in ms)
+		scroll_top_duration = 700,
+		//grab the "back to top" link
+		$back_to_top = $('.cd-top');
+
+	//hide or show the "back to top" link
+	$(window).scroll(function(){
+		( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
+		if( $(this).scrollTop() > offset_opacity ) { 
+			$back_to_top.addClass('cd-fade-out');
+		}
+	});
+
+	//smooth scroll to top
+	$back_to_top.on('click', function(event){
+		event.preventDefault();
+		$('body,html').animate({
+			scrollTop: 0 ,
+		 	}, scroll_top_duration
+		);
+	});
+
+});
+
+
+
+
